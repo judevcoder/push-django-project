0e0bcfde383ccbeeb3f39cc1b08b6668d0554081
Tudor Munteanu <tudor@toadlondon.com>
2016-12-10 01:07:57 +0000

    fix timezones

--- django_project/settings.py
+++ django_project/settings.py
@@ -108,13 +108,13 @@ else:
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Chicago'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 if IS_LOCAL:
     STATICFILES_DIRS = ( os.path.join(os.path.dirname(__file__), 'static'),)
