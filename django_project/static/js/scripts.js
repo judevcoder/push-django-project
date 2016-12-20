f42d5117e5e4fc4a003956b8c9171317ec3d2dfa
Tudor Munteanu <tudor@toadlondon.com>
2016-12-20 16:30:02 +0000

    add modal when testing the demo

--- django_project/static/js/scripts.js
+++ django_project/static/js/scripts.js
@@ -3,6 +3,7 @@ $(function(){
   $("#try-now-button").click(function(ev) {
 
     ev.stopPropagation();
+    $('#demo-modal').modal();
     $("head").append('<script src="/sdk/config-IZLUX5Y42DGPW1CS7.js"></script>');
   })
 });
