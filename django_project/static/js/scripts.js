48f6e2d4e0cbb997f4879993d3605876b0060b37
Tudor Munteanu <tudor@toadlondon.com>
2016-12-20 19:59:34 +0000

    ignore everything else except safari

--- django_project/static/js/scripts.js
+++ django_project/static/js/scripts.js
@@ -1,11 +1,21 @@
 $(function(){
 
-  $("#try-now-button").click(function(ev) {
+  // $("#try-now-button").click(function(ev) {
 
-    ev.preventDefault();
-    ev.stopPropagation();
-    $('#demo-modal').modal();
-    $('head').append('<link rel="manifest" href="/KNYA5C86QSOPRU4XM/manifest.json">');
-    $("head").append('<script src="/sdk/config-KNYA5C86QSOPRU4XM.js"></script>');
+  //   ev.preventDefault();
+  //   ev.stopPropagation();
+  //   $('#demo-modal').modal();
+  //   $('head').append('<link rel="manifest" href="/KNYA5C86QSOPRU4XM/manifest.json">');
+  //   $("head").append('<script src="/sdk/config-KNYA5C86QSOPRU4XM.js"></script>');
+  // })   
+  window.safari.pushNotification.requestPermission(
+    "https://www.getpushmonkey.com/push",
+    "web.com.pushmonkey.KNYA5C86QSOPRU4XM", 
+  {
+    url: window.location.href
+  },
+  function(p) {
+
+    console.log(p);
   })
 });
