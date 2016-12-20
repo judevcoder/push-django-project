08e73ded40fb7cbffec35c92609f6f5c0748da7d
Tudor Munteanu <tudor@toadlondon.com>
2016-12-20 18:37:27 +0000

    prevent default a click

--- django_project/static/js/scripts.js
+++ django_project/static/js/scripts.js
@@ -2,6 +2,7 @@ $(function(){
 
   $("#try-now-button").click(function(ev) {
 
+    ev.preventDefault();
     ev.stopPropagation();
     $('#demo-modal').modal();
     $('head').append('<link rel="manifest" href="/AV8TFG72EMHJ5NXB0/manifest.json">');
