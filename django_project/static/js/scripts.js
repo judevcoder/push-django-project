d3b0632ad2ed92503b4c2e8b0d8e23ca3015426d
Tudor Munteanu <tudor@toadlondon.com>
2016-12-20 17:01:15 +0000

    add manifest file

--- django_project/static/js/scripts.js
+++ django_project/static/js/scripts.js
@@ -4,6 +4,7 @@ $(function(){
 
     ev.stopPropagation();
     $('#demo-modal').modal();
+    $('head').append('<link rel="manifest" href="/IZLUX5Y42DGPW1CS7/manifest.json">');
     $("head").append('<script src="/sdk/config-IZLUX5Y42DGPW1CS7.js"></script>');
   })
 });
