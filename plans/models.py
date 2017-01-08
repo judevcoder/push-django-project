3eabc0877a9183bef6773d1217d2eb0fa3ba081b
Tudor Munteanu <tudor@toadlondon.com>
2017-01-08 12:13:54 +0000

    update price for starter plan

--- plans/models.py
+++ plans/models.py
@@ -66,12 +66,12 @@ def is_cool_update_offer():
 def calculate_starter_monthly():
     if is_cool_update_offer():
         return 3
-    return 14.99
+    return 3.99
 
 def calculate_starter_yearly():
     if is_cool_update_offer():
         return 29 
-    return 9.99 * 12
+    return 2.99 * 12
 
 def calculate_sweet_spot_monthly():
     if is_black_friday():
