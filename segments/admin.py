fdf847907e691e8572191f5b80aac0725a082b49
Tudor Munteanu <tudor@toadlondon.com>
2017-03-15 18:27:42 +0000

    update admin for segments

--- segments/admin.py
+++ segments/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from models import Segment
-from pushmonkey.models import Device
+from pushmonkey.models import Device, WebServiceDevice
 
 class SegmentAdmin(admin.ModelAdmin):
   def formfield_for_manytomany(self, db_field, request, **kwargs):
@@ -11,6 +11,13 @@ class SegmentAdmin(admin.ModelAdmin):
         kwargs["queryset"] = segment.device.all()
       else:
         kwargs["queryset"] = Device.objects.all()
+    elif db_field.name == "web_service_device":
+      pk = self.get_pk(request)
+      if pk:
+        segment = Segment.objects.get(id = pk)
+        kwargs["queryset"] = segment.web_service_device.all()
+      else:
+        kwargs["queryset"] = WebServiceDevice.objects.all()      
     return super(SegmentAdmin, self).formfield_for_manytomany(db_field, request, **kwargs)
 
   def get_pk(self, request):
